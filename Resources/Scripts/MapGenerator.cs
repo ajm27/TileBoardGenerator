9f264fb50c59a321d7a1ac338643ab75c5c60e3f
Alexander <ajm27@njit.edu>
2019-05-18 01:53:56 -0400

    2D array map generating

--- Resources/Scripts/MapGenerator.cs
+++ Resources/Scripts/MapGenerator.cs
@@ -6,21 +6,7 @@ using UnityEngine;
 public class MapGenerator : MonoBehaviour
 {
     public List<GameObject> TileDeck = new List<GameObject>();
-
-    // Tile Counts
-    public int NUMBER_OF_FOOD = 18;
-    public int NUMBER_OF_WOOD = 18;
-    public int NUMBER_OF_STONE = 18;
-    public int NUMBER_OF_METAL = 17;
-    public int NUMBER_OF_GOLD = 17;
-    public int NUMBER_OF_FOOD_WOOD = 11;
-    public int NUMBER_OF_WOOD_STONE = 11;
-    public int NUMBER_OF_FOOD_STONE = 11;
-    public int NUMBER_OF_METAL_GOLD = 10;
-    public int NUMBER_OF_FOOD_WOOD_STONE = 6;
-    public int NUMBER_OF_BLANK = 25;
-    public int NUMBER_OF_MOUNTAIN = 15;
-    public int NUMBER_OF_TOTAL_TILES;
+    public GameObject[,] ObscureMap;
 
     public GameObject FoodTile;
     public GameObject WoodTile;
@@ -35,14 +21,47 @@ public class MapGenerator : MonoBehaviour
     public GameObject BlankTile;
     public GameObject MountainTile;
 
+    // Tile Counts
+    public int NUMBER_OF_FOOD            = 18;
+    public int NUMBER_OF_WOOD            = 18;
+    public int NUMBER_OF_STONE           = 18;
+    public int NUMBER_OF_METAL           = 17;
+    public int NUMBER_OF_GOLD            = 17;
+    public int NUMBER_OF_FOOD_WOOD       = 11;
+    public int NUMBER_OF_WOOD_STONE      = 11;
+    public int NUMBER_OF_FOOD_STONE      = 11;
+    public int NUMBER_OF_METAL_GOLD      = 10;
+    public int NUMBER_OF_FOOD_WOOD_STONE = 6;
+    public int NUMBER_OF_BLANK           = 25;
+    public int NUMBER_OF_MOUNTAIN        = 15;
+    public int NUMBER_OF_TOTAL_TILES;
+
     float ColumnOffset = -0.800f;
-    int[] RowCounts = { 9, 10, 9, 8, 9, 10, 11, 10, 9, 8, 9, 10, 11, 10, 9, 8, 9, 10, 9 };
+    int[] RowCounts    = { 9, 10, 9, 8, 9, 10, 11, 10, 9, 8, 9, 10, 11, 10, 9, 8, 9, 10, 9 };
     float[] RowOffsets = { 0.0f, 0.4f, 0.0f, -0.4f, 0.0f, 0.4f, 0.8f, 0.4f, 0.0f, -0.4f, 0.0f, 0.4f, 0.8f, 0.4f, 0.0f, -0.4f, 0.0f, 0.4f, 0.0f };
+
+    int mapsize = 0;
+
+    //Offset Constants
+    float XOFFSET         =  0.675f;
+    float YOFFSET         = -0.784f;
+    float YOFFSET_ADJUST  = -0.393f;
+
+    List<Vector2> toCheck = new List<Vector2>()
+    {
+        new Vector2(-1, 0), // Top-Left
+        new Vector2( 0,-1), // Top
+        new Vector2( 1, 0), // Top-Right
+        new Vector2(-1, 1), // Bottom-Left
+        new Vector2( 0, 1), // Bottom
+        new Vector2( 1, 1)  // Bottom-Right
+    };
 
     // Start is called before the first frame update
     void Start()
     {
         //ReshuffleMap();
+        GenerateObscureMap();
     }
 
     void GenerateTileDeck()
@@ -53,106 +72,106 @@ public class MapGenerator : MonoBehaviour
         {
             GameObject tile = Instantiate(FoodTile);
             tile.GetComponent<Tile>().SetType(TILETYPE.FOOD);
-            tile.transform.position = new Vector2(0, 0);
+            tile.transform.position = new Vector2(1000, 1000);
             TileDeck.Add(tile);
             totalTiles++;
         }
 
-        for (int i = 0; i < NUMBER_OF_WOOD; i ++)
+        for (int i = 0; i < NUMBER_OF_WOOD; i++)
         {
             GameObject tile = Instantiate(WoodTile);
             tile.GetComponent<Tile>().SetType(TILETYPE.WOOD);
-            tile.transform.position = new Vector2(0, 0);
+            tile.transform.position = new Vector2(1000, 1000);
             TileDeck.Add(tile);
             totalTiles++;
         }
 
-        for (int i = 0; i < NUMBER_OF_STONE; i ++)
+        for (int i = 0; i < NUMBER_OF_STONE; i++)
         {
             GameObject tile = Instantiate(StoneTile);
             tile.GetComponent<Tile>().SetType(TILETYPE.STONE);
-            tile.transform.position = new Vector2(0, 0);
+            tile.transform.position = new Vector2(1000, 1000);
             TileDeck.Add(tile);
             totalTiles++;
         }
-        
-        for (int i = 0; i < NUMBER_OF_METAL; i ++)
+
+        for (int i = 0; i < NUMBER_OF_METAL; i++)
         {
             GameObject tile = Instantiate(MetalTile);
             tile.GetComponent<Tile>().SetType(TILETYPE.METAL);
-            tile.transform.position = new Vector2(0, 0);
+            tile.transform.position = new Vector2(1000, 1000);
             TileDeck.Add(tile);
             totalTiles++;
         }
-        
-        for (int i = 0; i < NUMBER_OF_GOLD; i ++)
+
+        for (int i = 0; i < NUMBER_OF_GOLD; i++)
         {
             GameObject tile = Instantiate(GoldTile);
             tile.GetComponent<Tile>().SetType(TILETYPE.GOLD);
-            tile.transform.position = new Vector2(0, 0);
+            tile.transform.position = new Vector2(1000, 1000);
             TileDeck.Add(tile);
             totalTiles++;
         }
-        
-        for (int i = 0; i < NUMBER_OF_FOOD_WOOD; i ++)
+
+        for (int i = 0; i < NUMBER_OF_FOOD_WOOD; i++)
         {
             GameObject tile = Instantiate(FoodWoodTile);
             tile.GetComponent<Tile>().SetType(TILETYPE.FOODWOOD);
-            tile.transform.position = new Vector2(0, 0);
+            tile.transform.position = new Vector2(1000, 1000);
             TileDeck.Add(tile);
             totalTiles++;
         }
-        
-        for (int i = 0; i < NUMBER_OF_WOOD_STONE; i ++)
+
+        for (int i = 0; i < NUMBER_OF_WOOD_STONE; i++)
         {
             GameObject tile = Instantiate(WoodStoneTile);
             tile.GetComponent<Tile>().SetType(TILETYPE.WOODSTONE);
-            tile.transform.position = new Vector2(0, 0);
+            tile.transform.position = new Vector2(1000, 1000);
             TileDeck.Add(tile);
             totalTiles++;
         }
-        
-        for (int i = 0; i < NUMBER_OF_FOOD_STONE; i ++)
+
+        for (int i = 0; i < NUMBER_OF_FOOD_STONE; i++)
         {
             GameObject tile = Instantiate(FoodStoneTile);
             tile.GetComponent<Tile>().SetType(TILETYPE.FOODSTONE);
-            tile.transform.position = new Vector2(0, 0);
+            tile.transform.position = new Vector2(1000, 1000);
             TileDeck.Add(tile);
             totalTiles++;
         }
-        
-        for (int i = 0; i < NUMBER_OF_METAL_GOLD; i ++)
+
+        for (int i = 0; i < NUMBER_OF_METAL_GOLD; i++)
         {
             GameObject tile = Instantiate(MetalGoldTile);
             tile.GetComponent<Tile>().SetType(TILETYPE.METALGOLD);
-            tile.transform.position = new Vector2(0, 0);
+            tile.transform.position = new Vector2(1000, 1000);
             TileDeck.Add(tile);
             totalTiles++;
         }
-        
-        for (int i = 0; i < NUMBER_OF_FOOD_WOOD_STONE; i ++)
+
+        for (int i = 0; i < NUMBER_OF_FOOD_WOOD_STONE; i++)
         {
             GameObject tile = Instantiate(FoodWoodStoneTile);
             tile.GetComponent<Tile>().SetType(TILETYPE.FOODWOODSTONE);
-            tile.transform.position = new Vector2(0, 0);
+            tile.transform.position = new Vector2(1000, 1000);
             TileDeck.Add(tile);
             totalTiles++;
         }
-        
-        for (int i = 0; i < NUMBER_OF_BLANK; i ++)
+
+        for (int i = 0; i < NUMBER_OF_BLANK; i++)
         {
             GameObject tile = Instantiate(BlankTile);
             tile.GetComponent<Tile>().SetType(TILETYPE.BLANK);
-            tile.transform.position = new Vector2(0, 0);
+            tile.transform.position = new Vector2(1000, 1000);
             TileDeck.Add(tile);
             totalTiles++;
         }
-        
-        for (int i = 0; i < NUMBER_OF_MOUNTAIN; i ++)
+
+        for (int i = 0; i < NUMBER_OF_MOUNTAIN; i++)
         {
             GameObject tile = Instantiate(MountainTile);
             tile.GetComponent<Tile>().SetType(TILETYPE.MOUNTAIN);
-            tile.transform.position = new Vector2(0, 0);
+            tile.transform.position = new Vector2(1000, 1000);
             TileDeck.Add(tile);
             totalTiles++;
         }
@@ -198,7 +217,96 @@ public class MapGenerator : MonoBehaviour
 
     void GenerateObscureMap()
     {
+        GenerateTileDeck();
+        RandomizeDeck();
 
+        mapsize = (int) Mathf.Sqrt(NUMBER_OF_TOTAL_TILES) + 2;
+        ObscureMap = new GameObject[mapsize, mapsize];
+
+        AssignSlots();
+    }
+
+    int placedTiles = 0;
+
+    public void AssignSlots()
+    {
+        foreach (GameObject tile in TileDeck)
+        {
+            Tile tileScript = tile.GetComponent<Tile>();
+
+            if (tileScript.Placed == false)
+            {
+                //Get random slot
+                Vector2 slot = new Vector2(Random.Range(0, mapsize), Random.Range(0, mapsize));
+
+                //Check if slot is null
+                if (ObscureMap[(int)slot.x, (int)slot.y] == null)
+                {
+                    //Decide whether tile is placed
+                    int rand = Random.Range(0, 5);
+                    if (rand == 1)
+                    {
+                        //calculate position
+                        Vector2 vector = new Vector2(XOFFSET * slot.x, (slot.x % 2 != 0) ? (YOFFSET * slot.y) + YOFFSET_ADJUST : (YOFFSET * slot.y));
+
+                        //place the tile
+                        tileScript.UpdatePosition(vector);
+                        tileScript.SetSlot(slot);
+                        tileScript.Placed = true;
+                        ObscureMap[(int)slot.x, (int)slot.y] = tile;
+
+                        placedTiles++;
+                    }
+                }
+                else
+                {
+                    List<Vector2> available = CheckForNeighbors(slot);
+
+                    foreach (Vector2 availableSlot in available)
+                    {
+                        //Decide whether tile is placed
+                        int rand = Random.Range(0, 4);
+                        if (rand == 1)
+                        {
+                            //calculate position
+                            Vector2 vector = new Vector2(XOFFSET * slot.x, (slot.x % 2 != 0) ? (YOFFSET * slot.y) + YOFFSET_ADJUST : (YOFFSET * slot.y));
+
+                            //place the tile
+                            tileScript.UpdatePosition(vector);
+                            tileScript.SetSlot(slot);
+                            tileScript.Placed = true; 
+                            ObscureMap[(int)slot.x, (int)slot.y] = tile;
+
+                            placedTiles++;
+                        }
+                    }
+                }
+            }
+
+        }
+
+        if (placedTiles != NUMBER_OF_TOTAL_TILES)
+            AssignSlots();
+    }
+
+    private List<Vector2> CheckForNeighbors(Vector2 slot)
+    {
+        List<Vector2> available = new List<Vector2>();
+
+        foreach(Vector2 check in toCheck)
+        {
+            int adjustedX = (int) (slot.x + check.x);
+            int adjustedY = (int) (slot.y + check.y);
+
+            if (!(adjustedX < 0 || adjustedX > mapsize - 1 || adjustedY < 0 || adjustedY > mapsize - 1 ))
+                if (ObscureMap[adjustedX, adjustedY] != null)
+                {
+                    Debug.Log($"Checking: { adjustedX } , { adjustedY }");
+                    available.Add(new Vector2(adjustedX, adjustedY));
+                }
+        }
+
+        return available;
     }
 
     public void ReshuffleMap()
--- Resources/Scripts/Tile.cs
+++ Resources/Scripts/Tile.cs
@@ -21,38 +21,13 @@ public enum TILETYPE
 
 public class Tile : MonoBehaviour
 {
-    MapGenerator mapgenerator;
+    public bool Placed = false;
     TILETYPE type;
-    Hashtable neighbors = new Hashtable();
-    bool full = false;
+    Vector2? slot = null;
 
-    Tile(MapGenerator mapgenerator, TILETYPE type)
+    Tile(TILETYPE type)
     {
-        this.mapgenerator = mapgenerator;
         this.type = type;
-    }
-
-    private void Start()
-    {
-        Vector2 pos = transform.position;
-
-        neighbors.Add(new Vector2(pos.x + 0, pos.y + 0.783f), null); //Top
-        neighbors.Add(new Vector2(pos.x + 0, pos.y - 0.783f), null); //Bottom
-        neighbors.Add(new Vector2(pos.x - 0.675f, pos.y + 0.391f), null); //Top-Left
-        neighbors.Add(new Vector2(pos.x + 0.675f, pos.y + 0.391f), null); //Top-Right
-        neighbors.Add(new Vector2(pos.x - 0.675f, pos.y - 0.783f), null); //Bottom-Left
-        neighbors.Add(new Vector2(pos.x + 0.675f, pos.y - 0.783f), null); //Bottom-Right
-
-        if (mapgenerator == null)
-            mapgenerator = Camera.main.GetComponent<MapGenerator>();
-
-        mapgenerator.GetTileDeck().Add(gameObject);
-    }
-
-    private void Update()
-    {
-        if(!full)
-            CheckForNeighbors(mapgenerator.GetTileDeck());
     }
 
     public void SetType(TILETYPE type)
@@ -65,45 +40,13 @@ public class Tile : MonoBehaviour
         transform.position = position;
     }
 
-    public bool CheckForNeighbors(List<GameObject> tileDeck)
+    public Vector2? GetSlot()
     {
-        foreach(GameObject tile in tileDeck)
-        {
-            if (!tile.name.Equals(gameObject.name))
-            {
-                //Debug.DrawLine(gameObject.transform.position, tile.transform.position, Color.red);
-                if (IsNeighbor(tile))
-                {
-                    Debug.Log(gameObject.name + " has neighbor " + tile.name);
-                }
-                else
-                {
-                    Debug.Log("Has no neighbors");
-                }
-            }
-        }
-
-        return false;
+        return slot;
     }
 
-    private bool IsNeighbor(GameObject tile)
+    public void SetSlot(Vector2 slot)
     {
-        CircleCollider2D collider = gameObject.GetComponent<CircleCollider2D>();
-
-        Vector2 origin = gameObject.transform.position;
-        Vector2 other_origin = tile.transform.position;
-
-        float distance = Mathf.Sqrt(Mathf.Pow(other_origin.x - origin.x, 2) + Mathf.Pow(other_origin.y - origin.y, 2));
-        float diameter = ((collider.radius * 2) / 8);
-
-        if (distance <= diameter)
-            return true;
-
-        return false;
-    }
-
-    public Vector2 FindFreeSpace()
-    {
-
+        this.slot = slot;
     }
 }
